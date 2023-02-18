aaee2a53dd5d8d089050896b6ffebf550018829a
brianl628 <brian@skucorp.net>
2023-02-18 02:31:27 -0600

    dailyreport

--- DailyReport/DailyReport/Program.cs
+++ DailyReport/DailyReport/Program.cs
@@ -20,7 +20,6 @@ namespace DailyReport
             Console.ReadLine();
             Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false.'");
             bool yourHelp = true;
-            string helpStatus = Convert.ToString(yourHelp);
             Console.ReadLine();
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string yourExperience = Console.ReadLine();

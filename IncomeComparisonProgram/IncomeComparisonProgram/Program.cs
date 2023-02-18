098df2c034ee36cfbbf87c7bdb48f27adc99cecc
brianl628 <brian@skucorp.net>
2023-02-18 02:37:50 -0600

    changes

--- IncomeComparisonProgram/IncomeComparisonProgram/Program.cs
+++ IncomeComparisonProgram/IncomeComparisonProgram/Program.cs
@@ -10,7 +10,7 @@ namespace IncomeComparisonProgram
             Console.WriteLine("Person 1");
             Console.WriteLine("What is your hourly rate?");
             string HourlyRate = Console.ReadLine();
-            int Rate = Convert.ToInt32(HourlyRate);
+            int Rate = Convert.ToInt32(HourlyRate); //convert to integer from string
             Console.WriteLine("How many hours worked per week?");
             string WeeklyHours = Console.ReadLine();
             int Hours = Convert.ToInt32(WeeklyHours);
@@ -22,15 +22,15 @@ namespace IncomeComparisonProgram
             string WeeklyHours2 = Console.ReadLine();
             int Hours2 = Convert.ToInt32(WeeklyHours2);
             Console.WriteLine("Annual Salary of Person 1");
-            int product = Rate * Hours;
+            int product = Rate * Hours; //multiplies values for annual salary
             Console.WriteLine(product);
             Console.WriteLine("Annual Salary of Person 2");
             int product2 = Rate2 * Hours2;
-            Console.WriteLine(product2);
+            Console.WriteLine(product2); 
             int Person1 = product;
             int Person2 = product2;
             Console.WriteLine("Does Person 1 make more money than Person 2?");
-            bool moreMoney = product > product2;
+            bool moreMoney = product > product2; //boolean statement
             Console.WriteLine(moreMoney);
             Console.ReadLine();
 

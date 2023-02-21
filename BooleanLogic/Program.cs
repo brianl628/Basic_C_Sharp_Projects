6686c511ded305d8f4d118332d3c041012c26a6e
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-02-21 02:26:01 -0600

    Add files via upload

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int Age = Convert.ToInt32(yourAge);
+            Console.WriteLine("Have you ever had a DUI?");
+            string yourDUI = Console.ReadLine();
+            bool noDUI = true;
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            int Tickets = Convert.ToInt32(yourTickets);
+            Console.WriteLine("Qualified?");
+            bool overAge = (Age > 15 && noDUI && Tickets == 0);
+            Console.ReadLine();
+            Console.WriteLine(overAge);
+        }
+    }
+}

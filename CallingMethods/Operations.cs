0428774b58b294507a12d663f993a7212e71c595
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-02-26 12:10:41 -0600

    calling methods

---
+++ CallingMethods/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Operation
+    {
+        public int Addition(int Num)
+        {
+            Num3 = Num + 10;
+            return Num3;
+        }
+
+        public int Division(int Num)
+        {
+            Num4 = Num / 10;
+            return Num4;
+        }
+
+        public int Multiplication(int Num)
+        {
+            Num5 = Num * 10;
+            return Num5;
+        }
+        int Num3 { get; set; }
+        int Num4 { get; set; }
+        int Num5 { get; set; }
+    }
+}
---
+++ CallingMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation mathObject = new Operation();
+            int Num3 = 10;
+            int Num4 = 11;
+            int Num5 = 12;
+            Console.WriteLine("Please enter a number.");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            Num3 = mathObject.Addition(Num);
+            Num4 = mathObject.Division(Num);
+            Num5 = mathObject.Multiplication(Num);
+
+            Console.WriteLine("Adding 10 to " + Num + " equals " + Num3);
+            Console.WriteLine("Dividing 10 to " + Num + " equals " + Num4);
+            Console.WriteLine("Multiplying 10 to " + Num + " equals " + Num5);
+
+        }
+    }
+}
+
+

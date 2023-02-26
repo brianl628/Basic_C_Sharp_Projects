5b1046a8a00c65bb62db6df35c3af34f68c00bfe
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-02-26 12:13:10 -0600

    Update Program.cs

--- CallingMethods/Program.cs
+++ CallingMethods/Program.cs
@@ -11,18 +11,18 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            Operation mathObject = new Operation();
+            Operation mathObject = new Operation(); 
             int Num3 = 10;
             int Num4 = 11;
             int Num5 = 12;
             Console.WriteLine("Please enter a number.");
-            int Num = Convert.ToInt32(Console.ReadLine());
+            int Num = Convert.ToInt32(Console.ReadLine()); //convert to integer
 
-            Num3 = mathObject.Addition(Num);
+            Num3 = mathObject.Addition(Num); //calling methods of operations
             Num4 = mathObject.Division(Num);
             Num5 = mathObject.Multiplication(Num);
 
-            Console.WriteLine("Adding 10 to " + Num + " equals " + Num3);
+            Console.WriteLine("Adding 10 to " + Num + " equals " + Num3); //produces output of each math operator
             Console.WriteLine("Dividing 10 to " + Num + " equals " + Num4);
             Console.WriteLine("Multiplying 10 to " + Num + " equals " + Num5);
 

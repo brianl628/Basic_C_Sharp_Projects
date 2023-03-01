11947ba6f9e1dcb9fe63876d4d65045e7e8b745c
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-01 04:39:49 -0600

    Update Program.cs

--- MethodSubmission/Program.cs
+++ MethodSubmission/Program.cs
@@ -6,13 +6,13 @@ namespace MethodSubmission
     {
         static void Main(string[] args)
         {
-            Function MathClass = new Function();
+            Function MathClass = new Function();    //new function added
             Console.WriteLine("Please enter a number.");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num1 = Convert.ToInt32(Console.ReadLine()); //convert to integer
             Console.WriteLine("Please enter a second number. You do not need to enter a second number if you don't want to.");
             string num2 = (Console.ReadLine());
 
-            if (num2.Equals(string.Empty))
+            if (num2.Equals(string.Empty))      //if 2nd number is empty, will follow this path
             {
                 Console.WriteLine("Your total comes out to " + MathClass.Multiply(num1));
             }

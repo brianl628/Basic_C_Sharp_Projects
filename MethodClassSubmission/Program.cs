3f99db674e5ec8cdc77deef31a0963fd560e132b
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-01 04:54:39 -0600

    methodclasssubmission

---
+++ MethodClassSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int Num1 = Convert.ToInt32(Console.ReadLine()); //convert to integer
+            Console.WriteLine("Please enter a second number.");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+
+            myClass Math = new myClass();
+            int result;
+            Math.Addition(Num1, Num2, out result);
+
+
+
+
+        }
+    }
+}
---
+++ MethodClassSubmission/myClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class myClass
+{
+	public void Addition (int Num1, int Num2, out int result)  //void method
+    {
+        result = Num1 + 200 - Num2; //function
+        Console.WriteLine("Your result is " + result); //display result
+    }
+
+}

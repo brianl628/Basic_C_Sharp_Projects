b3af484b3cff22dd25bb44ca0b7a8e769fa723bf
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-01 04:37:10 -0600

    methodsubmission

---
+++ MethodSubmission/Function.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodSubmission
+{
+    internal class Function
+    {
+        public int Multiply (int num1, int num2 = 100)
+        {
+            return (num1 * 2) * num2 + 300;
+        }
+        
+    }
+}
---
+++ MethodSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function MathClass = new Function();
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number. You do not need to enter a second number if you don't want to.");
+            string num2 = (Console.ReadLine());
+
+            if (num2.Equals(string.Empty))
+            {
+                Console.WriteLine("Your total comes out to " + MathClass.Multiply(num1));
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                Console.WriteLine("Your total comes out to " + MathClass.Multiply(num1, num3));
+            }
+
+        }
+    }
+}

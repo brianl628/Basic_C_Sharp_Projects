ad1b7c702ad05592b882d5a54b717c8635d034bf
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-01 06:29:23 -0600

    abstractclasscsubmission

---
+++ AbstractClassSubmission/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Employee : Person
+{
+	public override void SayName() //override method
+	{
+		base.SayName();
+	}
+}
---
+++ AbstractClassSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AbstractClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Person = new Employee() //new class
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            Person.SayName();
+            Console.ReadLine();
+        }
+    }
+}

c6788dc6e7231a49edd6d2e5b40dc105619b04d7
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-01 05:19:08 -0600

    classsubmission

---
+++ ClassSubmission/Function.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public class Function
+{
+    public void mathFunction(int input, out int output) //void method
+    {
+        output = input / 2; //divide by 2
+        Console.WriteLine("Your result is " + output); //output result
+    }
+}
---
+++ ClassSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassSubmission
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Function Math = new Function(); //create new class
+            Console.WriteLine("Please enter a number. It'll be divided by 2.");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            int result; 
+            Math.mathFunction(Num1, out result);
+            Console.ReadLine();
+
+        }
+    }
+}

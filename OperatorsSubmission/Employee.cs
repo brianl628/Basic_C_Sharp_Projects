8bf936a47a36741428df0cc5c0d86e640b4dec19
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-06 09:07:39 -0600

    operatorssubmission

---
+++ OperatorsSubmission/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Employee : Person
+{
+	public int ID { get; set; }
+	public static bool operator ==(Employee Employee1, Employee Employee2)
+    {
+		if (Employee1.ID == Employee2.ID) //check if IDs are equal
+        {
+            return Employee1.ID == Employee2.ID;
+        }
+        else
+        {
+            return false; //if different, produce false result
+        }
+    }
+    public static bool operator !=(Employee Employee1, Employee Employee2)
+    {
+        return Employee2 != Employee1; //overload
+    }
+
+    public override bool Equals(object obj)
+    {
+        Employee employee = obj as Employee;
+        if (employee == null)
+        {
+            return false;
+        }
+        return this.ID.Equals(employee.ID);
+    }
+    public override int GetHashCode()
+    {
+        return ID.GetHashCode();
+    }
+}
+	
+
---
+++ OperatorsSubmission/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee1 = new Employee() { FirstName = "John", LastName = "Smith", ID = 1 }; //instantiate
+            Employee Employee2 = new Employee() { FirstName = "Mary", LastName = "Saunders", ID = 2 };
+            Console.WriteLine(Employee1 == Employee2);
+            Console.ReadLine(); //Pause application
+        }
+    }
+}

7131359d000a0a8b3f3956d6c53469669064a617
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-06 07:58:50 -0600

    change

--- MethodClassSubmission/myClass.cs
+++ MethodClassSubmission/myClass.cs
@@ -4,8 +4,9 @@ public class myClass
 {
 	public void Addition (int Num1, int Num2, out int result)  //void method
     {
-        result = Num1 + 200 - Num2; //function
+        result = Num1 + 200 ; //function
         Console.WriteLine("Your result is " + result); //display result
+        Console.WriteLine("You're second number is " + Num2); //display 2nd number
     }
 
 }

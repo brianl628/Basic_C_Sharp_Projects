5e04d7d4855666cd601e56e4509a24490164f7c6
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-06 09:37:17 -0600

    Enumsubmission

---
+++ EnumSubmission/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EnumSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try //try + catch block
+            {
+                Console.Write("Please enter a day of the week.");
+                string input = Console.ReadLine();
+                DaysofWeek yourInput = (DaysofWeek)Enum.Parse(typeof(DaysofWeek), input, true); //parse enum
+            }
+            catch //catch if input is anything other than a day
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+        public enum DaysofWeek 
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}

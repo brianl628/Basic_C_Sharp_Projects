180172d832ac6d9aedc222501d64f4947ae7eac3
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-09 01:29:17 -0600

    constructorsubmission

---
+++ ConstructorSubmission/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Employee
+{
+	public string name;
+	public string status;
+	public Employee(string name) : this(name, "Employed") { } //chain constructors
+	public Employee (string name, string status) 
+    {
+		this.name = name;
+		this.status = status;
+    }
+}
---
+++ ConstructorSubmission/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Corporation = "The Office"; //new constructor
+            var newEmployee = new Employee("Johnny"); //using var to create new variable
+            Console.WriteLine("This is {0}", Corporation); //calls constructor string
+            Console.WriteLine(newEmployee.name + " is employed.");
+        }
+    }
+}

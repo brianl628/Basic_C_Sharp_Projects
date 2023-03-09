3c1db43a87ca1c33a31d5370cd9f21514384db69
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-09 00:40:46 -0600

    dateandtimesubmission

---
+++ DateAndTimeSubmission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateAndTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now; //list current date/time
+            Console.WriteLine("The current date/time is " + currentTime);
+            Console.WriteLine("Please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //convert to integer
+            Console.WriteLine("The time in X hours is " + currentTime.AddHours(userInput)); //add x hours to current time
+            Console.ReadLine();
+        }
+    }
+}

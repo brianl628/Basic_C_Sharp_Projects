3f5640686993548421abf0be93d2b3fadc768d24
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-03-27 04:23:12 -0500

    final challenge

---
+++ FinalChallenge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Data.Entity;
+
+namespace FinalChallenge
+{
+
+    
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades
+        {
+            get; set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Molly" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                var list = from a in ctx.Students
+                           
+                            select a;
+                foreach (var item in list)
+                {
+                    Console.WriteLine("You have added " + item.StudentName + " to the database.");
+                }
+
+            }
+        }
+    }
+}
+
